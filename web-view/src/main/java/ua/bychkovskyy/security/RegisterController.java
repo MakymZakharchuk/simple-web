4ea3ddf760aeba72829aee5a1ee3a63657d19ea7
Vyacheslav Bychkovskyy <v.bychkovskyy@gmail.com>
2013-10-01 23:28:03 +0300

    fix

--- web-view/src/main/java/ua/bychkovskyy/security/RegisterController.java
+++ web-view/src/main/java/ua/bychkovskyy/security/RegisterController.java
@@ -3,6 +3,7 @@ package ua.bychkovskyy.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
